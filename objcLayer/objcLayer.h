86076c31a3db6d5687c1484ed2df51046dec1b23
vilyever <250450035@163.com>
2016-08-05 16:56:20 +0800

    first release

--- objcLayer/objcLayer.h
+++ objcLayer/objcLayer.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char objcLayerVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <objcLayer/PublicHeader.h>
 
-
+#import <objcLayer/CALayer+VDShadow.h>
